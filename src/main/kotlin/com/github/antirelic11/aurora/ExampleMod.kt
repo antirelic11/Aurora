f9cd3649e6d67036d6d7ed5079c55fc7d9223d1b
GitHub Action <action@github.com>
2024-10-23 13:14:43 +0000

    Initialize template repository proper
    Set package name to com.github.antirelic11.aurora
    Set project name to Aurora
    Set modid to aurora

--- src/main/kotlin/com/github/antirelic11/aurora/ExampleMod.kt
+++ src/main/kotlin/com/github/antirelic11/aurora/ExampleMod.kt
@@ -1,4 +1,4 @@
-package com.example
+package com.github.antirelic11.aurora
 
 import net.minecraft.client.Minecraft
 import net.minecraft.init.Blocks
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.event.FMLInitializationEvent
 import net.minecraft.client.renderer.GlStateManager
 
-@Mod(modid = "examplemod", useMetadata = true)
+@Mod(modid = "aurora", useMetadata = true)
 class ExampleMod {
     @Mod.EventHandler
     fun init(event: FMLInitializationEvent) {
